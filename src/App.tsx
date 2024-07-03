5f1be727d90aaf011bdf9b6d7a58676255ea2a3c
SAY10s <nikodemmajzner@gmail.com>
2024-07-03 16:40:12 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import SaunaConfigurationView from "./views/SaunaConfigurationView.tsx";
+
+function App() {
+
+  return (
+    <>
+        <h1>Hello World!</h1>
+        <SaunaConfigurationView />
+    </>
+  )
+}
+
+export default App
---
+++ src/views/SaunaConfigurationView.tsx
@@ -0,0 +1,9 @@
+const SaunaConfigurationView = () => {
+    return (
+        <div>
+            <h2>Sauna Configuration</h2>
+        </div>
+    );
+};
+
+export default SaunaConfigurationView;
