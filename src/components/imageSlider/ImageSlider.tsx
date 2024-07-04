a64e24ea8f9f0d2b853e584632ba6bafbc6cea8d
SAY10s <nikodemmajzner@gmail.com>
2024-07-04 18:23:20 +0200

    The SliderWrapper now is a proper slider (the "show-on-option-change" functionality doesn't work yet)

--- src/components/imageSlider/ImageSlider.tsx
+++ src/components/imageSlider/ImageSlider.tsx
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store.ts";
+import { CURRENT_CONFIG_STEP_NAME_IN_ENGLISH } from "../../const/const.ts";
 
-interface PropsInterface {
-  imgURL: string;
+interface PropsInter {
+  imgURLs: string[];
 }
 
-const ImageSlider: React.FC<PropsInterface> = ({ imgURL }) => {
+const ImageSlider: React.FC<PropsInter> = ({ imgURLs }) => {
+  const currentConfigStepId = useSelector(
+    (state: RootState) => state.saunaConfig.currentConfigStepId,
+  );
+  const currentChosenOption = useSelector(
+    (state: RootState) =>
+      state.saunaConfig[
+        CURRENT_CONFIG_STEP_NAME_IN_ENGLISH[currentConfigStepId]
+      ],
+  );
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % imgURLs.length);
+  };
+
+  const goToPrevious = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + imgURLs.length) % imgURLs.length,
+    );
+  };
+
   return (
     <div className="imageSlider">
-      <img src={imgURL} alt="Zdjęcie obecnie wybranej opcji w danym kroku" />
+      <button onClick={goToPrevious}>&lt;</button>
+      <img src={imgURLs[currentIndex]} alt={`Slide ${currentIndex}`} />
+      <button onClick={goToNext}>&gt;</button>
     </div>
   );
 };
--- src/components/imageSlider/ImageSliderWrapper.tsx
+++ src/components/imageSlider/ImageSliderWrapper.tsx
@@ -2,7 +2,7 @@ import ImageSlider from "./ImageSlider.tsx";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store/store.ts";
 import {
-  CURRENT_CONFIG_STEP_NAME_IN_ENGLISH,
+  CONFIGURATION_OPTIONS,
   CURRENT_CONFIG_STEP_NAME_IN_POLISH,
 } from "../../const/const.ts";
 
@@ -10,20 +10,17 @@ const ImageSliderWrapper = () => {
   const currentConfigStepId = useSelector(
     (state: RootState) => state.saunaConfig.currentConfigStepId,
   );
-  const currentChosenOption = useSelector(
-    (state: RootState) =>
-      state.saunaConfig[
-        CURRENT_CONFIG_STEP_NAME_IN_ENGLISH[currentConfigStepId]
-      ],
-  );
+
   const chosenModel = useSelector(
     (state: RootState) => state.saunaConfig.model,
   );
 
   const currentConfigStepNameInPolish =
     CURRENT_CONFIG_STEP_NAME_IN_POLISH[currentConfigStepId];
+  const currentStepOptions = CONFIGURATION_OPTIONS[currentConfigStepId];
 
-  const imgURL = `https://placehold.co/256?text=${currentConfigStepNameInPolish}:+${currentChosenOption}`;
+  const imgURLs = currentStepOptions.map((option) => option.image);
+
   return (
     <div className="imageSliderWrapper grid65to35">
       {currentConfigStepId === 8 ? (
@@ -32,7 +29,7 @@ const ImageSliderWrapper = () => {
             <h4>Podsumowanie:</h4>
             <h1>{chosenModel}</h1>
           </div>
-          <ImageSlider imgURL={imgURL} />
+          <ImageSlider imgURLs={imgURLs} />
         </div>
       ) : (
         <div className="innerSliderElementWrapper">
@@ -40,7 +37,7 @@ const ImageSliderWrapper = () => {
             <h4>Opcja:</h4>
             <h1>{currentConfigStepNameInPolish}</h1>
           </div>
-          <ImageSlider imgURL={imgURL} />
+          <ImageSlider imgURLs={imgURLs} />
         </div>
       )}
     </div>
